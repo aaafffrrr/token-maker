d6a233d3f18be8e93a3ebb8820ca64fe4e293be2
aaafffrrr <yvitiragif@gmail.com>
2024-06-28 18:45:04 -0700

    Update stripe_card_token_flask.py

--- stripe_card_token_flask.py
+++ stripe_card_token_flask.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import stripe
+import os
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
@@ -36,4 +37,5 @@ def create_token():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
